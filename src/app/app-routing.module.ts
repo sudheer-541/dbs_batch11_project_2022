99658167f6e55266c18bdf8ca47e6973132ffe4a
Sudheer <bukkasamudram.sssreddy@gmail.com>
2022-08-24 07:07:31 +0000

    Front end flow with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReceiverComponent } from './receiver/receiver.component';
+import { TransactionComponent } from './transaction/transaction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'receiver',component: ReceiverComponent},
+  {path: 'transaction',component: TransactionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
